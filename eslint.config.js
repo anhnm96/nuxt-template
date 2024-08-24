007e1de924793502c16baa3321e7777026c9658b
Nguyen Minh Anh <xiaosasori@gmail.com>
2024-08-24 21:46:45 +0700

    chore: remove prettier

--- eslint.config.js
+++ eslint.config.js
@@ -6,5 +6,6 @@ export default antfu({
     'no-console': ['warn', { allow: ['info', 'warn', 'error'] }],
     'antfu/if-newline': 'off',
     'node/prefer-global/process': 'off',
+    'style/brace-style': ['error', '1tbs'],
   },
 })
