7c51d79107c3f67f2d23c4192a1c46996d6bf8d9
Nguyen Minh Anh <xiaosasori@gmail.com>
2025-03-25 20:29:04 +0700

    style(Tabs): fluid animation

--- app/components/tab/Tabs.vue
+++ app/components/tab/Tabs.vue
@@ -14,9 +14,10 @@ export const [provideDialogRootContext, injectDialogRootContext]
 </script>
 
 <script setup lang="ts">
-const props = defineProps<{
+const props = withDefaults(defineProps<{
   vertical?: boolean
-}>()
+  duration?: number
+}>(), { duration: 150 })
 
 const tabsId = useId()
 
@@ -26,15 +27,56 @@ const orientation = computed(() =>
 
 const modelValue = defineModel<PrimitiveValue>('value', { required: true })
 const activeItem = reactive({ size: 0, position: 0 })
+
+let previousSize = -1
+let previousPosition = 0
+let animated = true
+
+function handleFluidMove(targetSize: number, targetPosition: number) {
+  if (!animated) {
+    return
+  }
+
+  animated = false
+
+  if (previousSize === -1) {
+    activeItem.size = targetSize
+    activeItem.position = targetPosition
+
+    animated = true
+  } else {
+    if (targetPosition > previousPosition) {
+      activeItem.size = targetSize + targetPosition - previousPosition
+
+      setTimeout(() => {
+        activeItem.size = targetSize
+        activeItem.position = targetPosition
+
+        animated = true
+      }, props.duration)
+    } else {
+      activeItem.position = targetPosition
+      activeItem.size = previousSize + previousPosition - activeItem.position
+
+      setTimeout(() => {
+        activeItem.size = targetSize
+
+        animated = true
+      }, props.duration)
+    }
+  }
+
+  previousSize = targetSize
+  previousPosition = targetPosition
+}
+
 function selectTab(value: PrimitiveValue, el: HTMLElement) {
   modelValue.value = value
 
   if (orientation.value === 'vertical') {
-    activeItem.size = el.getBoundingClientRect().height
-    activeItem.position = el.offsetTop
+    handleFluidMove(el.getBoundingClientRect().height, el.offsetTop)
   } else {
-    activeItem.size = el.getBoundingClientRect().width
-    activeItem.position = el.offsetLeft
+    handleFluidMove(el.getBoundingClientRect().width, el.offsetLeft)
   }
 }
 
